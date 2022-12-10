6aca44a3c4382c08894ee0d61e255035d26c66cb
Jack <nonepassby@163.com>
2022-12-10 15:59:12 +0800

    fix issue: failed to update conatainer property: set bool properties value "true" to true

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ mkdir -p /opt/tools/etc/
 [[ -f /opt/tools/etc/smartos-docker.conf ]] || cp src/smartos-docker.conf /opt/tools/etc/smartos-docker.conf
 cp src/smartos-docker /opt/tools/bin/smartos-docker
 chmod +x /opt/tools/bin/smartos-docker
-ln -s /opt/tools/bin/smartos-docker /opt/tools/bin/docker
+ln -sf /opt/tools/bin/smartos-docker /opt/tools/bin/docker
